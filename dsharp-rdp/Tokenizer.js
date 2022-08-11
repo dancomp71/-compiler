be96e0ef5aa84fd9ab5199ee4e45a2cca1fd499e
Daniel Serrano <dancomp71@aol.com>
2022-08-10 22:10:01 -0400

    added regex in a loop

--- dsharp-rdp/Tokenizer.js
+++ dsharp-rdp/Tokenizer.js
@@ -1,3 +1,20 @@
+const Spec = [
+    // whitespace
+    [/^\s+/, null],
+    
+    // numbers
+    [/^\d+/, 'NUMBER'],
+    
+    //comments
+    [/^\/\/.*/, null],
+    [/^\/\*[\s\S]*?\*\//, null],
+
+    // strings
+    [/^"[^"]*"/, 'STRING'],
+    [/^'[^']*'/, 'STRING'],
+];
+
+
 class Tokenizer {
     init(string) {
         this._string = string;
@@ -8,66 +25,40 @@ class Tokenizer {
         return this._cursor < this._string.length;
     }
 
+    _match(regexp, string) {
+        const matched = regexp.exec(string);
+        if(matched == null) {
+            return null;
+        }
+        this._cursor += matched[0].length;
+        return matched[0];
+    }
+
     getNextToken() {
         if(!this.hasMoreTokens()) {
             return null;
         }
 
         const string = this._string.slice(this._cursor);
+        for(const [regexp, tokenType] of Spec) {
+            const tokenValue = this._match(regexp, string);
+            
+            if (tokenValue == null)  {
+                continue;
+            }
 
-        let matched = /^\d+/.exec(string);
-        if(matched !== null) {
-            this._cursor += matched[0].length;
+            if(tokenType == null) {
+                // whitespace
+                return this.getNextToken();
+            }
+
             return {
-                type: 'NUMBER',
-                value: matched[0],
+                type: tokenType,
+                value: tokenValue
             };
         }
 
-        matched = /^"[^"]*"/.exec(string);
-        if(matched !== null) {
-            this._cursor += matched[0].length;
-            return {
-                type: 'STRING',
-                value: matched[0],
-            };
-        }
-
-        matched = /^'[^']*'/.exec(string);
-        if(matched !== null) {
-            this._cursor += matched[0].length;
-            return {
-                type: 'STRING',
-                value: matched[0],
-            };
-        }
-        // Numbers:
-
-        // if(!Number.isNaN(Number(string[0]))) {
-        //     let number = '';
-        //     while(this._cursor < this._string.length && !Number.isNaN(Number(string[this._cursor]))) {
-        //         number += string[this._cursor++];
-        //     }
-        //     return {
-        //         type: 'NUMBER',
-        //         value: number,
-        //     };
-        // }
-
-        // // String:
-        // if(string[0] === '"') {
-        //     let s = '';
-        //     do {
-        //         s += string[this._cursor++];
-        //     } while(string[this._cursor]!== '"' && !this.isEOF());
-        //     s += string[this._cursor++]; // skip "
-        //     return {
-        //         type: 'STRING',
-        //         value: s,
-        //     };
-        // }
-
-        return null;
+        throw new SyntaxError(`Unexpected token: "${string[0]}"`);
     }
 
     isEOF() {
--- dsharp-rdp/__tests__/run.js
+++ dsharp-rdp/__tests__/run.js
@@ -1,9 +1,20 @@
 const { Parser } = require('../Parser.js');
 const parser = new Parser();
 
-let program = `42`;
+let program = `   42  `;
 let ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
+
+program = `   
+    // NUMBERS
+92  
+    /*hello
+    ** wahtever
+    */
+`;
+ast = parser.parse(program);
+console.log(JSON.stringify(ast, null, 2));
+
 
 program = `"hello"`;
 ast = parser.parse(program);
@@ -14,5 +25,9 @@ ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
 
 program = `'52'`;
+ast = parser.parse(program);
+console.log(JSON.stringify(ast, null, 2));
+
+program = `'   5292  '`;
 ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
