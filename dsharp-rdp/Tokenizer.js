8f14b11ba43c2bb55454bdc6a551032685563a7b
Daniel Serrano <dancomp71@aol.com>
2022-08-10 21:42:20 -0400

    added single quote and regexpr

--- dsharp-rdp/Tokenizer.js
+++ dsharp-rdp/Tokenizer.js
@@ -15,30 +15,57 @@ class Tokenizer {
 
         const string = this._string.slice(this._cursor);
 
-        // Numbers:
-        if(!Number.isNaN(Number(string[0]))) {
-            let number = '';
-            while(this._cursor < this._string.length && !Number.isNaN(Number(string[this._cursor]))) {
-                number += string[this._cursor++];
-            }
+        let matched = /^\d+/.exec(string);
+        if(matched !== null) {
+            this._cursor += matched[0].length;
             return {
                 type: 'NUMBER',
-                value: number,
+                value: matched[0],
             };
         }
 
-        // String:
-        if(string[0] === '"') {
-            let s = '';
-            do {
-                s += string[this._cursor++];
-            } while(string[this._cursor]!== '"' && !this.isEOF());
-            s += string[this._cursor++]; // skip "
+        matched = /^"[^"]*"/.exec(string);
+        if(matched !== null) {
+            this._cursor += matched[0].length;
             return {
                 type: 'STRING',
-                value: s,
+                value: matched[0],
             };
         }
+
+        matched = /^'[^']*'/.exec(string);
+        if(matched !== null) {
+            this._cursor += matched[0].length;
+            return {
+                type: 'STRING',
+                value: matched[0],
+            };
+        }
+        // Numbers:
+
+        // if(!Number.isNaN(Number(string[0]))) {
+        //     let number = '';
+        //     while(this._cursor < this._string.length && !Number.isNaN(Number(string[this._cursor]))) {
+        //         number += string[this._cursor++];
+        //     }
+        //     return {
+        //         type: 'NUMBER',
+        //         value: number,
+        //     };
+        // }
+
+        // // String:
+        // if(string[0] === '"') {
+        //     let s = '';
+        //     do {
+        //         s += string[this._cursor++];
+        //     } while(string[this._cursor]!== '"' && !this.isEOF());
+        //     s += string[this._cursor++]; // skip "
+        //     return {
+        //         type: 'STRING',
+        //         value: s,
+        //     };
+        // }
 
         return null;
     }
--- dsharp-rdp/__tests__/run.js
+++ dsharp-rdp/__tests__/run.js
@@ -1,23 +1,18 @@
 const { Parser } = require('../Parser.js');
-
 const parser = new Parser();
 
 let program = `42`;
-
 let ast = parser.parse(program);
-
 console.log(JSON.stringify(ast, null, 2));
-
 
 program = `"hello"`;
-
 ast = parser.parse(program);
-
 console.log(JSON.stringify(ast, null, 2));
 
-
 program = `"42"`;
-
 ast = parser.parse(program);
+console.log(JSON.stringify(ast, null, 2));
 
+program = `'52'`;
+ast = parser.parse(program);
 console.log(JSON.stringify(ast, null, 2));
